327f714304c9f950a8d1f6431dc87f876f17e05d
Roman Dalinkevich <romandalinkevich@gmail.com>
2022-08-29 18:50:27 +0300

    Adding a timer

--- form.cpp
+++ form.cpp
@@ -1,12 +1,26 @@
 #include "form.h"
 #include "ui_form.h"
 #include <QDebug>
+#include <QTimer>
+#include <QTime>
+#include <QtGui>
+#include <QtWidgets>
 
 Form::Form(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::Form)
 {
     ui->setupUi(this);
+
+    //Центрирование окна
+    QDesktopWidget desktop;
+    QRect rect = desktop.availableGeometry(this);
+    QPoint center = rect.center();
+    int x = center.x() - (width() / 2);
+    int y = center.y() - (height() / 2);
+    center.setX(x);
+    center.setY(y);
+    move(center);
 
 }
 
@@ -20,6 +34,7 @@ void Form::slotToTwoWindow(QString login)
 {
     ui->resultLabel->setText("Пользователь-  " + login + "  Авторизовался");
     qDebug() << "второе окно дебаг " << login;
+    ui->time->setText("Первое окно было закрыто: " + QTime::currentTime().toString("hh:mm:ss"));
 
 }
 
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,33 +2,53 @@
 #include "ui_mainwindow.h"
 #include <QMessageBox>
 #include <QDebug>
-#include <QPixmap> //отображение изображения
+#include <QPixmap>
+#include <QTimer>
+#include <QTime>
+#include <QtGui>
+#include <QtWidgets>
 
-
-//при генерации сигналов firstSignal, secondSignal  в классе MainWindow, в классах FirstClass, SecondClass будут запускаться слоты firstSlot secondSlot
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    QPixmap pix(":/Resource/Images/Authorize.png"); // путь к изображению
+
+    //Центрирование окна
+    QDesktopWidget desktop;
+    QRect rect = desktop.availableGeometry(this);
+    QPoint center = rect.center();
+    int x = center.x() - (width() / 2);
+    int y = center.y() - (height() / 2);
+    center.setX(x);
+    center.setY(y);
+    move(center);
+
+    //Лого
+    QPixmap pix(":/Resource/Images/Authorize.png");
     int w = ui->logo->width();
     int h = ui->logo->height();
 
     ui->logo->setPixmap(pix.scaled(w,h, Qt::KeepAspectRatio)); // помещаем изображение в logo + изображение растягивается при изменении размера
 
-    //экземпляры классов
+    //Экземпляры классов
     firstClass = new FirstClass();
     secondClass = new SecondClass();
     form = new Form();
+    timer = new QTimer();
 
 
+    //Время запуска приложения
+    qDebug() << QTime::currentTime().toString("hh:mm:ss") << endl;
 
-    //Новый метод connect
+
     connect(this, &MainWindow::firstSignal, firstClass, &FirstClass::firstSlot);
     connect(this, &MainWindow::secondSignal, secondClass, &SecondClass::secondSlot);
     connect(this, &MainWindow::signalToTwoWindow, form, &Form::slotToTwoWindow);
+    connect(timer, SIGNAL(timeout()), this, SLOT(slotTimerAlarm()));
+
+    timer->start(1000);
 }
 
 MainWindow::~MainWindow()
@@ -36,7 +56,8 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-// сигналы
+
+
 void MainWindow::test()
 {
     qDebug() << "Слот test запущен" << endl;
@@ -45,31 +66,43 @@ void MainWindow::test()
 
 }
 
+
 void MainWindow::showToWindow()
 {
     emit signalToTwoWindow(ui->login->text());
 }
 
 
+//Таймер
+void MainWindow::slotTimerAlarm()
+{
+     QTime::currentTime().toString("hh:mm:ss");
+}
+
+
 
 void MainWindow::on_pushButton_clicked()
 {
-
     test();
-
 
     QString login = ui->login->text();
     QString password = ui->password->text();
 
     if(login == "123456" && password == "qwerty") {
         ui->statusbar->showMessage("Вы успешно авторизовались");
+
         form->show();
         this->close();
 
+        timer->stop();
         showToWindow();
 
 
    } else {
          ui->statusbar->showMessage("Авторизация не успешна");
    }
+
 }
+
+
+
